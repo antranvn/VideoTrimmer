7cfde5356cfa2f12f651a444a9cfbf699135cb5e
antranvn <andevstudioth@gmail.com>
2020-07-10 14:35:08 +0700

    Refactor source code.

--- app/src/main/java/com/lb/video_trimmer_sample/MainActivity.kt
+++ app/src/main/java/com/lb/video_trimmer_sample/MainActivity.kt
@@ -80,6 +80,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == REQUEST_VIDEO_TRIMMER) {
                 val uri = data!!.data
